f88451f0f74ad58b02cc5fa556ee67851d3bc8f9
Alexander <pchp250g8@tutamail.com>
2026-03-04 11:22:05 +0300

    Update HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -9,5 +9,8 @@ int _tmain(int argc, _TCHAR* argv[])
     clrscr(cout);
     cout << setcrsrtype(0) << setxy(10,10)
     	 << "Hello,World!!!";
+    cout << setcrsrtype(0) << setxy(11,10)
+    	 << "Conio Library";
     getch();
 }
+
